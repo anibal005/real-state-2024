aecda9d60083a8a1447d80928d4cc35b97a6ffcc
Anibal <anibaldel005@gmail.com>
2024-06-27 15:57:37 -0400

    sign up page ui completado

--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>
-      
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>
+        Registrarse
+      </h1>
+      <form className='flex flex-col gap-4 '>
+        <input type="text" placeholder='nombre de usuario' className='border p-3 rounded-lg' id='userame'/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
+        <input type="password" placeholder='conttraseña' className='border p-3 rounded-lg' id='password'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          Registrarse
+        </button>
+        <div className='flex gap-2 mt-5'>
+          <p>Tienes una cuenta?</p>
+          <Link to="/sig-in">
+            <span className='text-blue-700'>Iniciar Sesión</span>
+          </Link>
+        </div>
+      </form>
     </div>
   )
 }
