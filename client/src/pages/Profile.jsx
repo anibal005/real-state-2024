ee86f857d4cfe4147dc7512b99c83804f351ee20
Anibal <anibaldel005@gmail.com>
2024-07-05 09:25:09 -0400

    profile page UI completado

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Profile() {
+  const {currentUser} = useSelector(state=> state.user)
   return (
-    <div>
-      Profile
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>
+        Perfil
+      </h1>
+      <form className='flex flex-col gap-4'>
+        <img 
+          src={currentUser.avatar} 
+          alt="profile" 
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+          <input type="text" placeholder='Nombre de usuario' id='username' className='border p-3 rounded-lg'/>
+          <input type="email" placeholder='Email' id='email' className='border p-3 rounded-lg'/>
+          <input type="password" placeholder='Contraseña' id='password' className='border p-3 rounded-lg'/>
+          <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80'>
+            actualizar
+          </button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>eliminar cuenta</span>
+        <span className='text-red-700 cursor-pointer'>cerrar sesión</span>
+      </div>
     </div>
   )
 }
