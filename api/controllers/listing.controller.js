1b0aa67fdfc099e09d831d47ddb153ecd6e3b008
Anibal <anibaldel005@gmail.com>
2024-07-15 15:36:02 -0400

    delete listing user completo

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
 
 export const createListing=async(req,res,next)=> {
     try {
@@ -7,4 +8,23 @@ export const createListing=async(req,res,next)=> {
     } catch (error) {
         next(error)
     }
+}
+
+export const deleteListing = async (req, res ,next)=> {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+        return next(errorHandler(404, 'Listado no encontrado'))
+    }
+
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'solo puedes eleiminar tu propio listado'))
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id)
+        res.status(200).json('El listado ha sido eliminado')
+    } catch (error) {
+        next(error)
+    }
+
 }
